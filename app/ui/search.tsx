df9b509672018a65242cd2ff47275c0f83713dcb
Abdullateef Bello <belloabdullateef035@gmail.com>
2025-04-09 12:48:38 +0100

    Add debouncing

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
@@ -13,7 +14,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   console.log("searchParams.get", searchParams.get("query"));
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
     console.log(`Searching... ${term}`);
     // URLSearchParams is a Web API that providesutility methods for manipulating the URL query parameters
     const params = new URLSearchParams(searchParams); // copied the current search params for modification
@@ -23,7 +24,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete("query");
     }
     replace(`${pathName}?${params.toString()}`);
-  }
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
