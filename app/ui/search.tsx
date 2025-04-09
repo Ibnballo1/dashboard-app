606918f9bb2cf60d6666f681f3b0f7bb5be03938
Abdullateef Bello <belloabdullateef035@gmail.com>
2025-04-09 12:37:35 +0100

    add search feature

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,29 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const { replace } = useRouter();
+  // useSearchParams is a hook that returns the current URL search parameters as a URLSearchParams object.
+  // usePathname is a hook that returns the current URL pathname as a string.
+  // useRouter is a hook that returns the router object, which provides methods for navigating and manipulating the URL.
+
+  console.log("searchParams.get", searchParams.get("query"));
+
+  function handleSearch(term: string) {
+    console.log(`Searching... ${term}`);
+    // URLSearchParams is a Web API that providesutility methods for manipulating the URL query parameters
+    const params = new URLSearchParams(searchParams); // copied the current search params for modification
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathName}?${params.toString()}`);
+  }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +32,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
