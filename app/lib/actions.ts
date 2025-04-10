68015ce2a47fa1cec17d4c26bf0f373d06d87306
Abdullateef Bello <belloabdullateef035@gmail.com>
2025-04-10 16:44:30 +0100

    Create new invoices

---
+++ app/lib/actions.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import { z } from "zod";
+import { revalidatePath } from "next/cache";
+import postgres from "postgres";
+import { redirect } from "next/navigation";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+// Define the schema for the form data
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+// The Server Action to be called when form is submitted
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  const amountInCent = amount * 100; // convert the amount to cent to avoid js floating-points error
+  const date = new Date().toISOString().split("T")[0]; // get the current date in YYYY-MM-DD format
+
+  await sql`
+    INSERT INTO invoices (customer_Id, amount, status, date)
+    VALUES (${customerId}, ${amountInCent}, ${status}, ${date})
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
