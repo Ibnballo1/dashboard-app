3595e39444a26c76ba2cb1186a3ffde052b1bc85
Abdullateef Bello <belloabdullateef035@gmail.com>
2025-04-13 21:40:39 +0100

    Add update to invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -17,7 +17,7 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
-
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 // The Server Action to be called when form is submitted
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
@@ -31,6 +31,25 @@ export async function createInvoice(formData: FormData) {
   await sql`
     INSERT INTO invoices (customer_Id, amount, status, date)
     VALUES (${customerId}, ${amountInCent}, ${status}, ${date})
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCent = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCent}, status = ${status}
+    WHERE id = ${id}
   `;
 
   revalidatePath("/dashboard/invoices");
