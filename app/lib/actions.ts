4aecd143b26a2625983fb0c114c93a3de6686764
Abdullateef Bello <belloabdullateef035@gmail.com>
2025-04-15 11:55:31 +0100

    add form validation for create

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -10,21 +10,45 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 // Define the schema for the form data
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({
+    invalid_type_error: "Please select a customer",
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than $0." }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "please select an invoice status",
+  }),
   date: z.string(),
 });
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 // The Server Action to be called when form is submitted
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing validatedFields, failed to create invoie",
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCent = amount * 100; // convert the amount to cent to avoid js floating-points error
   const date = new Date().toISOString().split("T")[0]; // get the current date in YYYY-MM-DD format
 
@@ -34,7 +58,10 @@ export async function createInvoice(formData: FormData) {
     VALUES (${customerId}, ${amountInCent}, ${status}, ${date})
   `;
   } catch (error) {
-    console.error("Error creating invoice:", error);
+    // console.error("Error creating invoice:", error);
+    return {
+      message: "Database error: Failed to Create Invoice",
+    };
   }
 
   revalidatePath("/dashboard/invoices");
